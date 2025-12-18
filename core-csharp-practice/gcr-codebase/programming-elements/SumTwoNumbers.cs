0e638bf0bcf43fb2a5813554155e1b3a62485f40
Aman290103 <amankumarchaubey2901@gmail.com>
2025-12-18 15:16:49 +0530

    [ Aman Kumar Chaubey ] Add. Added program to calculate sum of two numbers

---
+++ core-csharp-practice/gcr-codebase/programming-elements/SumTwoNumbers.cs
@@ -0,0 +1,13 @@
+using System;
+
+class SumTwoNumbers
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        Console.WriteLine("Sum = " + sum);
+    }
+}
