be6c1f3fbf6d8d2c039e64fc58c014ae369cb826
Aman290103 <amankumarchaubey2901@gmail.com>
2025-12-18 15:13:47 +0530

    [ Aman Kumar Chaubey ] Add. Added program to find perimeter of rectangle

---
+++ core-csharp-practice/gcr-codebase/programming-elements/PerimeterOfRectangle.cs
@@ -0,0 +1,13 @@
+using System;
+
+class PerimeterOfRectangle
+{
+    static void Main(string[] args)
+    {
+        double length = 10;
+        double width = 5;
+
+        double perimeter = 2 * (length + width);
+        Console.WriteLine("Perimeter = " + perimeter);
+    }
+}
