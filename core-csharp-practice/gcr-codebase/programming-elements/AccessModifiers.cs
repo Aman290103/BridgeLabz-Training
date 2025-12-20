a15a3d4dde481f6040117a875fbb8942d171c9b4
Aman290103 <amankumarchaubey2901@gmail.com>
2025-12-20 10:51:21 +0530

    "[ Aman Kumar Chaubey ] Add. Added a program which consist all Access Modifiers in C#"

---
+++ core-csharp-practice/gcr-codebase/programming-elements/AccessModifiers.cs
@@ -0,0 +1,50 @@
+using System;
+
+// PUBLIC class → accessible everywhere
+public class BaseClass
+{
+    // PRIVATE → only inside BaseClass
+    private int privateValue = 10;
+
+    // PROTECTED → BaseClass + DerivedClass
+    protected int protectedValue = 20;
+
+    // INTERNAL → same project
+    internal int internalValue = 30;
+
+    // PUBLIC → accessible everywhere
+    public int publicValue = 40;
+
+    // PUBLIC method to show private member
+    public void PrivateMethod()
+    {
+        Console.WriteLine("Private Value: " + privateValue);
+    }
+}
+
+// DERIVED CLASS
+public class SubClass : BaseClass
+{
+    public void ProtectedMethod()
+    {
+        Console.WriteLine("Protected Value: " + protectedValue);
+    }
+}
+
+// MAIN CLASS
+class AccessModifiersDemo
+{
+    public static void Main()
+    {
+        BaseClass obj = new BaseClass();
+
+        // Accessible
+        Console.WriteLine("Public Value: " + obj.publicValue);
+        Console.WriteLine("Internal Value: " + obj.internalValue);
+
+        // Private not accessible directly
+        obj.PrivateMethod();
+
+      ;
+    }
+}
