ff96350fd6a62a8fe83f122df18b07cbbca66916
Aman290103 <amankumarchaubey2901@gmail.com>
2026-01-08 11:49:05 +0530

    "[ Aman Kumar Chaubey ] Add. Added Smart Home Automation System scenario-based problem'

---
+++ oops-csharp-practice/scenario-based/Smart Home Automation System/AC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Smart_Home_Automation_System
+{
+    public class AC : Appliance, IControllable
+    {
+        public AC(string name) : base(name) { }
+        public void TurnOn()
+        {
+            Console.WriteLine($"{name} AC is cooling the room to 22°C.");
+        }
+        public void TurnOff()
+        {
+            Console.WriteLine($"{name} AC is turned OFF.");
+		}
+    
+    }
+}
---
+++ oops-csharp-practice/scenario-based/Smart Home Automation System/SmartHomeAutomation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Smart_Home_Automation_System
+{
+    public class SmartHomeAutomation
+    {
+        public static void Main(string[] args)
+        {
+            IControllable livingRoomLight = new Light("Living Room");
+            IControllable bedroomFan = new Fan("Bedroom");
+            IControllable kitchenAC = new AC("Kitchen");
+            SmartHomeUtility.TurnOnDevice(livingRoomLight);
+            SmartHomeUtility.TurnOnDevice(bedroomFan);
+            SmartHomeUtility.TurnOnDevice(kitchenAC);
+            SmartHomeUtility.TurnOffDevice(livingRoomLight);
+            SmartHomeUtility.TurnOffDevice(bedroomFan);
+            SmartHomeUtility.TurnOffDevice(kitchenAC);
+		}
+	}
+}
