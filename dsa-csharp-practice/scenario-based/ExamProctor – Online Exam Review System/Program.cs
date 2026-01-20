af216e0341c5dd4eb85345925d7a1d02f585ac1b
Aman290103 <amankumarchaubey2901@gmail.com>
2026-01-20 11:08:45 +0530

    "[ Aman Kumar Chaubey ] Add. Added ExamProctor – Online Exam Review System scenario based problem"

---
+++ dsa-csharp-practice/scenario-based/ExamProctor – Online Exam Review System/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dsa_scenario.ExamProctor___Online_Exam_Review_System
+{
+	public class Program
+	{
+		static void Main()
+		{
+			// Correct answers
+			var correctAnswers = new Dictionary<int, string>
+			{
+				{1, "A"},
+				{2, "B"},
+				{3, "C"}
+			};
+
+			// Start exam session
+			StudentScore session = new StudentScore(correctAnswers);
+
+			// Navigation + Answers
+			session.visitQuestion(1);
+			session.submitAnswer(1, "A");
+
+			session.visitQuestion(2);
+			session.submitAnswer(2, "B");
+
+			session.visitQuestion(3);
+			session.submitAnswer(3, "D"); // Wrong answer
+
+			// Calculate score
+			int score = session.CalculateScore();
+
+			Console.WriteLine("\nFinal Score: " + score + "/3");
+		}
+	}
+}
---
+++ dsa-csharp-practice/scenario-based/ExamProctor – Online Exam Review System/StudentScoreUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsa_scenario.ExamProctor___Online_Exam_Review_System
+{
+    public class StudentScoreUtility 
+    {
+		public static int Evaluate(Dictionary<int, string> studentAnswers, Dictionary<int, string> correctAnswers)
+		{
+            int score = 0;
+
+            foreach (var question in correctAnswers)
+            {
+                if (studentAnswers.ContainsKey(question.Key) && studentAnswers[question.Key].Equals(question.Value))
+                {
+                    score++;
+                }
+            }
+
+            return score;
+        }
+    }
+}
