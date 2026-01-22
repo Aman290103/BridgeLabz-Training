63f59caebaae306301a012d654a8da64413efbf0
Aman290103 <amankumarchaubey2901@gmail.com>
2026-01-22 22:43:53 +0530

    "[ Aman Kumar Chaubey ] Add. Added csharp-streams lab practice problems."

---
+++ collections-csharp-practice/gcr-codebase/csharp-streams/PipedStreams.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+class PipedStreams{
+    public static void Main(){
+        string file = "student.dat";
+        using (BinaryWriter bw = new BinaryWriter(File.Open(file, FileMode.Create))){
+            bw.Write(1);
+            bw.Write("Aman");
+            bw.Write(7.58);
+        }
+        using (BinaryReader br = new BinaryReader(File.Open(file, FileMode.Open))){
+            Console.WriteLine(br.ReadInt32());
+            Console.WriteLine(br.ReadString());
+            Console.WriteLine(br.ReadDouble());
+        }
+    }
+}
