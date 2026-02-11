e28d8586bfa2603b68fd66caa80eec7a5f0139fb
Aman290103 <amankumarchaubey2901@gmail.com>
2026-02-11 11:28:14 +0530

    "[ Aman Kumar Chaubey ] Add. Implemented all the uc's upto uc-12 with collections successfully."

---
+++ collections-csharp-practice/scenario-based/AddressBook System/AddressBookMenu.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.AddressBook_System
+{
+	sealed class AddressBookMenu
+	{
+		private IAddressBook addressBook;
+
+		public AddressBookMenu()
+		{
+			addressBook = new AddressBookUtilityImpl();
+		}
+
+		public void ShowMenu()
+		{
+			while (true)
+			{
+				Console.WriteLine("\n1. Add Contact");
+				Console.WriteLine("2. Edit Contact");
+				Console.WriteLine("3. Delete Contact");
+				Console.WriteLine("4. Search Person by City or State");
+				Console.WriteLine("5. View Persons by City or State");
+				Console.WriteLine("6. Count Contacts by City or State");
+				Console.WriteLine("7. Sort Contacts Alphabetically");
+				Console.WriteLine("8. Sort Contacts by City");
+				Console.WriteLine("9. Sort Contacts by State");
+				Console.WriteLine("10. Sort Contacts by Zip");
+
+
+				Console.WriteLine("11. Exit");
+
+				int choice = Convert.ToInt32(Console.ReadLine());
+
+				switch (choice)
+				{
+					case 1:
+						addressBook.AddContact();
+						break;
+
+					case 2:
+						addressBook.EditContact();
+						break;
+
+					case 3:
+						addressBook.DeleteContact();
+						break;
+
+						//-------------uc - 8-----------//
+					case 4:
+						addressBook.SearchPersonByCityOrState();
+						break;
+
+						//------------UC -9----------------//
+
+					case 5:
+						addressBook.ViewPersonsByCityOrState();
+						break;
+
+					case 6:
+    						addressBook.CountPersonsByCityOrState();
+						break;
+
+					case 7:
+    						addressBook.SortContactsByName();
+    						break;	
+
+					//----------UC12-----------------//		
+					case 8:
+    						addressBook.SortByCity();
+    						break;
+
+					case 9:
+						addressBook.SortByState();
+						break;
+
+					case 10:
+						addressBook.SortByZip();
+						break;
+
+					case 11:
+						Console.WriteLine("Exiting...");
+						return;
+
+				}
+			}
+		}
+
+	}
+}
+
+
+
