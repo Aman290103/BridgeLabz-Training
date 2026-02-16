14cba9f5ce637ed16118c7494e3dfe24b58ad710
Aman290103 <amankumarchaubey2901@gmail.com>
2026-02-16 15:52:59 +0530

    "[ Aman Kumar Chaubey ] Add. Added EmailValidator , Factory Robot hazard Analyzer scenario based problem."

---
+++ collections-csharp-practice/scenario-based/EmailValidator/EmailProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.EmailValidator
+{
+    public class EmailProcessor
+    {
+		private static readonly object fileLock = new object();
+
+		public static void processEmails(List<string> emails)
+		{
+			try
+			{
+				List<Task> tasks = new List<Task>();
+
+				foreach (var email in emails)
+				{
+					tasks.Add(Task.Run(() => validateAndSave(email)));
+				}
+
+				Task.WaitAll(tasks.ToArray());
+
+				Console.WriteLine("All emails processed.");
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("An error occurred while processing emails: " + ex.Message);
+			}
+
+		}
+
+		public static void validateAndSave(string email)
+		{
+			bool isValid = EmailValidator.validateEmail(email);
+			string fileName = isValid ? "valid_emails.txt" : "invalid_emails.txt";
+
+			lock (fileLock)
+			{
+				File.AppendAllText(fileName, email + Environment.NewLine);
+			}
+
+            Console.WriteLine($"{email} -> {(isValid?"Valid":"InValid")}");
+		}
+	}
+}
---
+++ collections-csharp-practice/scenario-based/EmailValidator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.EmailValidator
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            try
+            {
+                Console.WriteLine("-------EduConnect Email Validator------");
+                Console.WriteLine("Enter number of emails to validate:");
+
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                List<string> emails = new List<string>();
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine($"Enter email{i + 1}: ");
+                    emails.Add(Console.ReadLine());
+
+                }
+                EmailProcessor.processEmails(emails);
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid nnumber entered");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Unexpected Error: " + ex.Message);
+            }
+        }
+    }
+}
