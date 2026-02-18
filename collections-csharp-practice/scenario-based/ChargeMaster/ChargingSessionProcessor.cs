2955772c8f356303fe6b1cd56e1188b24a5e5b4d
Aman290103 <amankumarchaubey2901@gmail.com>
2026-02-19 00:10:46 +0530

    "[ Aman Kumar Chaubey ] Add. Added ChargeMaster scenario based problem."

---
+++ collections-csharp-practice/scenario-based/ChargeMaster/ChargingSessionProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review.ChargeMaster
+{
+    public abstract class ChargingSessionProcessor
+    {
+        protected int startBattery {  get; set; }
+        protected int endBattery { get; set; }
+        protected int duration {  get; set; }
+        protected string mode { get; set; }
+
+        public ChargingSessionProcessor(int start, int end , int duration,string mode)
+        {
+            this.startBattery = start;
+            this.endBattery = end;
+            this.duration = duration;
+            this.mode = mode;   
+        }
+
+        public abstract void validateSession();
+        public abstract double calculateChargingCost();
+
+        public double processSession(int start , int end , int duration , string mode)
+        {
+            validateSession();
+            return calculateChargingCost();
+        }
+
+
+	}
+}
